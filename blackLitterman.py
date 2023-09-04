e25b72d60b4d9d0442416e164aa713b8f7a4a5bf
tnalsdh21 <79125278+tnalsdh21@users.noreply.github.com>
2023-09-04 16:53:36 +0900

    Update blackLitterman.py

--- blackLitterman.py
+++ blackLitterman.py
@@ -2,12 +2,12 @@ def runBlack():
 
   result1 = {'type':'안정형',
             'us':1, 'uk':1, 'jp':1, 'euro':1,
-            'kor':1,'in':1, 'tw':1,'br':1,
+            'kor':1,'ind':1, 'tw':1,'br':1,
             'kor3y':1,'kor10y':1,'us3y':1, 'us10y':1,
             'gold':1}
   result2 = {'type':'위험형',
              'us': 1, 'uk': 1, 'jp': 1, 'euro': 1,
-            'kor': 1, 'in': 1, 'tw': 1, 'br': 1,
+            'kor': 1, 'ind': 1, 'tw': 1, 'br': 1,
             'kor3y': 1, 'kor10y': 1, 'us3y': 1, 'us10y': 1,
             'gold': 1}
   return result1, result2
